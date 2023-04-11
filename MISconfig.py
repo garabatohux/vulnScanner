df49c1d50c51d9d5de4dc0c1bcfcae762491d1a7
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-11 15:14:14 -0500

    upd

--- MISconfig.py
+++ MISconfig.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import os
-import sys
-from core.time import now
-from core.utility import generate_random_token
-
-
-def nettacker_paths():
-    """
-    home path for the framework (could be modify by user)
-
-    Returns:
-        a JSON contain the working, tmp and results path
-    """
-    return {
+[!]: 
+$ /usr/bin/env python3
+#-*- coding: utf-8 -*-
+`
+>>> import os
+>>> import sys
+    from core.time import now
+    from core.utility import generate_random_token
+`
+        def nettacker_paths():
+['HOME']:["PATH"]: 
+        for _$_LOAD(MOD) --usr
+            Returns:
+>>> import mod.JSON --docker \container\work-flow\tmp*result || \path
+    return 
+{
         "requirements_path": os.path.join(sys.path[0], 'requirements.txt'),
         "requirements_dev_path": os.path.join(sys.path[0], 'requirements-dev.txt'),
         "home_path": os.path.join(sys.path[0]),
@@ -30,67 +29,62 @@ def nettacker_paths():
         "web_static_files_path": os.path.join(sys.path[0], 'web/static'),
         "payloads_path": os.path.join(sys.path[0], 'lib/payloads'),
         "module_protocols_path": os.path.join(sys.path[0], 'core/module_protocols'),
-    }
-
-
-def nettacker_api_config():
-    """
+};
+`
+>>> def nettacker_api_config():
     API Config (could be modify by user)
-
+`
     Returns:
-        a JSON with API configuration
-    """
-    return {  # OWASP Nettacker API Default Configuration
+>>> mod.JSON with API.MISconfig
+    return 
+{  
+OWASP Nettacker API Default Configuration
         "start_api_server": False,
         "api_hostname": "0.0.0.0" if os.environ.get("docker_env") == "true" else "nettacker-api.z3r0d4y.com",
         "api_port": 5000,
         "api_debug_mode": False,
         "api_access_key": generate_random_token(32),
-        "api_client_whitelisted_ips": [],  # disabled - to enable please put an array with list of ips/cidr/ranges
-        # [
-        #     "127.0.0.1",
-        #     "10.0.0.0/24",
-        #     "192.168.1.1-192.168.1.255"
-        # ],
-        "api_access_log": os.path.join(sys.path[0], '.data/nettacker.log'),
-    }
-
-
-def nettacker_database_config():
-    """
-    Database Config (could be modified by user)
-    For sqlite database:
-        fill the name of the DB as sqlite,
-        DATABASE as the name of the db user wants
-        other details can be left empty
-    For mysql users:
-        fill the name of the DB as mysql
-        DATABASE as the name of the database you want to create
-        USERNAME, PASSWORD, HOST and the PORT of the MySQL server
-        need to be filled respectively
-
+        "api_client_whitelisted_ips": [NUL]:  
+disabled - to enable please put an array with list of ips/cidr/ranges
+["127.0.0.1","10.0.0.0/24","192.168.1.1-192.168.1.255"]:
+<?php 
+    "api_access_log": os.path.join(sys.path[0], '.data/nettacker.log'),
+    ?>
+};
+`
+>>> def nettacker_database_config(0):
+`
+>>> dat.config(mod) --usr
+>>> sqlite.dat \db:
+    name*db as sqlite
+        db as name*usr
+            {void};
+            for ["USR"]:
+                db as MYSQL
+                db as name*db --mod
+[+]: usr,pwd,["HOST","PORT"]:&&***MySQL:0
+`
     Returns:
-        a JSON with Database configuration
-    """
+mod.JSON with db.misConfig
+`    
     return {
         "DB": "sqlite",
         # "DB":"mysql", "DB": "postgres"
         "DATABASE": nettacker_paths()["database_path"],
-        # Name of the database
+        # Name*db
         "USERNAME": "",
         "PASSWORD": "",
         "HOST": "",
         "PORT": ""
-    }
-
-
-def nettacker_user_application_config():
-    """
+};
+`
+>>> def nettacker_user_application_config(1):
+`    
     core framework default config (could be modify by user)
-
+`
     Returns:
         a JSON with all user default configurations
-    """
+`    
     from core.compatible import version_info
     return {  # OWASP Nettacker Default Configuration
         "language": "en",
@@ -131,13 +125,13 @@ def nettacker_user_application_config():
         "show_all_modules": False,
         "show_all_profiles": False,
         "modules_extra_args": None
-    }
-
-
-def nettacker_global_config():
-    return {
+};
+`
+>>> def nettacker_global_config():
+        return {
         "nettacker_paths": nettacker_paths(),
         "nettacker_api_config": nettacker_api_config(),
         "nettacker_database_config": nettacker_database_config(),
         "nettacker_user_application_config": nettacker_user_application_config()
-    }
+};
+`
