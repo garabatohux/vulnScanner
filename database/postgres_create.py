1fb2ceba66358b22cbb4fd27ffbcd80748b212ca
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-12 07:44:28 -0500

    Update postgres_create.py

--- database/postgres_create.py
+++ database/postgres_create.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from sqlalchemy import create_engine
-from config import nettacker_database_config
-from database.models import Base
-from sqlalchemy.exc import OperationalError
-
+[!]:
+    $ /usr/bin/env python3
+-*- coding: utf-8 -*-
+`
+>>> from sqlalchemy import create_engine
+>>> from config import nettacker_database_config
+>>> from database.models import Base
+>>> from sqlalchemy.exc import OperationalError
+`
 USER = nettacker_database_config()["USERNAME"]
 PASSWORD = nettacker_database_config()["PASSWORD"]
 HOST = nettacker_database_config()["HOST"]
 PORT = nettacker_database_config()["PORT"]
 DATABASE = nettacker_database_config()["DATABASE"]
-
-
-def postgres_create_database():
+`
+>>> def postgres_create_database():
     """
-    when using postgres database, this is the function that is used to create the database for the first time when you
-    the nettacker run module.
-
+    postgres.db__This(f)-create_db for {1};time__Your_mod code.exe
+`
     Args:
         None
-
+`
     Returns:
-        True if success otherwise False
+        $True if $False ["SUCCESS"]
     """
-
+`
     try:
         engine = create_engine(
             'postgres+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(USER, PASSWORD, HOST, PORT, DATABASE)
