bdaede3b4087e5f952af14c6be04fffcfea54bab
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-12 08:13:35 -0500

    Update and rename readme.md to args.c

---
+++ lib/args.c
@@ -0,0 +1,12 @@
+[!]: 
+$ lib/c
+[+]MOD || LIB
+* `argparse` FIX for --!--unicode
+* `brute` type.js --mod
+* `graph` graph.js --mod 
+* `html_log` log.html
+* `icmp` libs
+* `language` msg 
+* `scan` mod.js
+* `vuln` m  o  d
+* `socks_resolver` resolve address By Burp_Suite(c)
