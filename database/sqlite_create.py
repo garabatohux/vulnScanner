bac51baad7cd94f5957b7ff90e955cb3bdce3886
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-12 07:35:24 -0500

    Update sqlite_create.py

--- database/sqlite_create.py
+++ database/sqlite_create.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from sqlalchemy import create_engine
-
-from database.models import Base
-from config import nettacker_database_config
-
+[!]: 
+    $ /usr/bin/env python3
+-*- coding: utf-8 -*-
+`
+>>> from sqlalchemy import create_engine
+`
+>>> from database.models import Base
+>>> from config import nettacker_database_config
+`
 DATABASE = nettacker_database_config()["DATABASE"]
-
-
-def sqlite_create_tables():
+`
+>>> def sqlite_create_tables():
     """
-    when using sqlite database, this is the function that is used to create the database schema for the first time when
-    you run the nettacker module.
-
+    Your(SQLite.db) This(f)-create--db_schema<xml>for{1};time.Your.exe \mod
+`
     Args:
         None
-
+`
     Returns:
-        True if success otherwise False
+        $True if $False ["SUCCESS"]
     """
     try:
         db_engine = create_engine(
