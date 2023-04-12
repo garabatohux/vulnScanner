a7c3dce848fae673a227be356a277f18dbc387d2
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-12 06:54:32 -0500

    Update and rename readme.md to readme.c

---
+++ database/readme.c
@@ -0,0 +1,7 @@
+[!]: \OWASP\nettacker\db\
+=========c o n t a i n e r / h a n d l e r / d b /==============
+For mod
+`db.py`db(f)
+`mod.py`dbSTRUCT-LAY-OUT
+`mysql_create.py`(f)-create db STRUCT(sqlite).db
+`sqlite_create.py`(f)-create db__STRUCT(MySQL).db
